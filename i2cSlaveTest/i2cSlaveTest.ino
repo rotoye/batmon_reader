c7cbb5b2bc09d7e6df6bb953574f3f8a6214c7df
echoG <518314+echoGee@users.noreply.github.com>
2018-03-29 11:44:48 -0400

    Adding Master slave test code: works

---
+++ i2cSlaveTest/i2cSlaveTest.ino
@@ -0,0 +1,37 @@
+#include <Wire.h>
+
+
+// Include the required Wire library for I2C<br>#include <Wire.h>
+int LED = 13;
+int x = 0;
+void setup() {
+  Serial.begin(115200);
+  // Define the LED pin as Output
+  pinMode (LED, OUTPUT);
+  // Start the I2C Bus as Slave on address 9
+  Wire.begin(9); 
+  // Attach a function to trigger when something is received.
+  Wire.onReceive(receiveEvent);
+  Serial.println("Wire started");
+}
+void receiveEvent(int bytes) {
+  x = Wire.read();    // read one character from the I2C
+}
+void loop() {
+  //If value received is 0 blink LED for 200 ms
+  Serial.println(x);
+  if (x == '0') {
+    digitalWrite(LED, HIGH);
+    delay(200);
+    digitalWrite(LED, LOW);
+    delay(200);
+  }
+  //If value received is 3 blink LED for 400 ms
+  if (x == '3') {
+    digitalWrite(LED, HIGH);
+    delay(400);
+    digitalWrite(LED, LOW);
+    delay(400);
+  }
+  delay(1);
+}
